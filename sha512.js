838ce1d0d7a0632f2b33741267c4bacd2e4e28b3
Bruno Mazzardo <bruno.mazzardo52@gmail.com>
2018-10-09 17:16:14 -0300

    Mudando import no index

--- sha512.js
+++ sha512.js
@@ -6,17 +6,19 @@
 'use strict';
 
 const {NativeModules} = require('react-native')
-const sha512Lib = NativeModules.sha512Lib;
-
+const sha512lib = NativeModules.sha512lib;
+console.warn(Object.keys(NativeModules))
 
 
 export default {
 
-  sha256 :(data) => {
-    return sha512Lib.sha256(data)
-  },
-  sha512: (data) => {
-    return sha512Lib.sha512(data)
-  }
+
+    sha256 :(data) => {
+        return sha512lib.sha256(data)
+    },
+
+    sha512: (data) => {
+        return sha512lib.sha512(data)
+    }
 
 }
